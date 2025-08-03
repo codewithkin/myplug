aada078d3cee6622ac68c61b44e54c8cdc69f9a6
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-03 18:59:28 +0200

    [ADD]: ThemeProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
-import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/ui/theme-provider'
 
 const poppins = Poppins({
   subsets: ['latin'],
