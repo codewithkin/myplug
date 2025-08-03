af1773ec7095ca0c3c7f573b5365d025772120a8
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-03 19:58:47 +0200

    [ADD]: Dark mode styles to ModeToggle

--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -18,9 +18,9 @@ export function ModeToggle() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button className="rounded-full" variant="outline" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+                <Button className="rounded-full dark:bg-slate-900 dark:hover:bg-slate-700" variant="outline" size="icon">
+                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 " />
+                    <Moon className="absolute  h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
