73a746fedf18aedc591198ab9cf5d3c56fa66b5b
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-04 19:34:35 +0200

    [ADD]: Update .gitignore to include .cursor file

--- src/helpers/uploads/s3.ts
+++ src/helpers/uploads/s3.ts
@@ -2,11 +2,11 @@
 import { S3Client } from "bun";
 
 const s3Client = new S3Client({
-    accessKeyId: process.env.SEVALLA_ACCESS_KEY_ID,
-    secretAccessKey: process.env.SEVALLA_SECRET_ACCESS_KEY,
-    bucket: process.env.SEVALLA_BUCKET,
-    endpoint: process.env.SEVALLA_ENDPOINT,
-    region: process.env.SEVALLA_REGION
+    accessKeyId: Bun.env.SEVALLA_ACCESS_KEY_ID,
+    secretAccessKey: Bun.env.SEVALLA_SECRET_ACCESS_KEY,
+    bucket: Bun.env.SEVALLA_BUCKET,
+    endpoint: Bun.env.SEVALLA_ENDPOINT,
+    region: Bun.env.SEVALLA_REGION
 });
 
 export default s3Client;
