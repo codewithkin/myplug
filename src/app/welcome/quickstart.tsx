b7910dc0e46b63349bc47831a6fd7f8bd17f1e25
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-04 13:32:58 +0200

    [UPDATE]: Enhance QuickStart component styles for dark mode support and improve button aesthetics

--- src/app/welcome/quickstart.tsx
+++ src/app/welcome/quickstart.tsx
@@ -7,12 +7,12 @@ import QuickStartSteps from "./quickStartSteps";
 export default function QuickStart() {
   
     return (
-        <article className="flex shadow-md w-full border rounded-xl border-blue-500 flex-col items-center w-full p-4 justify-center gap-4">
+        <article className="flex shadow-md w-full border rounded-xl border-blue-500 dark:border-blue-900 flex-col items-center w-full p-4 justify-center gap-4">
             <article className="w-full flex items-center justify-between">
             <h2 className="text-lg font-semibold">Let's get you started</h2>
 
-            <Link href="#" className="flex gap-2 text-blue-500 items-center">
-                <Video size={16} strokeWidth={1.8} />
+            <Link href="#" className="flex gap-2 text-blue-500 dark:text-blue-800 font-semibold items-center">
+                <Video size={16} strokeWidth={2} />
 
                 <p className="text-sm">Watch tutorials</p>
             </Link>
