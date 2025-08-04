323dd46bf03ae1928182979b62455eefaa07b3f9
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-04 19:56:27 +0200

    [UPDATE]: Change S3 bucket reference to use process environment variable for improved configuration

--- src/helpers/uploads/uploadFile.ts
+++ src/helpers/uploads/uploadFile.ts
@@ -3,7 +3,7 @@ import s3Client from "./s3";
 
 export default async function uploadFile(file: File) {
     const dataFileUrl = await s3Client.send(new PutObjectCommand({
-        Bucket: Bun.env.SEVALLA_BUCKET,
+        Bucket: process.env.SEVALLA_BUCKET,
         Key: `chatbots/${file.name}`,
         Body: file
     }));
