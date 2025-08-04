0217a60bd0d5536d2f91ab5e082d8e62a79ae2c9
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-04 18:46:03 +0200

    [UPDATE]: Refine QuickStart component styles for dark mode, adjusting border and text colors

--- src/app/welcome/quickstart.tsx
+++ src/app/welcome/quickstart.tsx
@@ -7,11 +7,11 @@ import QuickStartSteps from "./quickStartSteps";
 export default function QuickStart() {
   
     return (
-        <article className="flex shadow-md w-full border rounded-xl border-blue-500 dark:border-blue-900 flex-col items-center w-full p-4 justify-center gap-4">
+        <article className="flex shadow-md w-full border rounded-xl border-blue-500 dark:border-slate-700 flex-col items-center w-full p-4 justify-center gap-4">
             <article className="w-full flex items-center justify-between">
             <h2 className="text-lg font-semibold">Let's get you started</h2>
 
-            <Link href="#" className="flex gap-2 text-blue-500 dark:text-blue-800 font-semibold items-center">
+            <Link href="#" className="flex gap-2 text-blue-500 dark:text-slate-700 font-semibold items-center">
                 <Video size={16} strokeWidth={2} />
 
                 <p className="text-sm">Watch tutorials</p>
