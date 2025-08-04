3db5345cee7771b6f99368abe8e6cb9da0f2c804
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-04 19:45:39 +0200

    [UPDATE]: Refactor S3 client implementation to use AWS SDK and add uploadFile helper function for file uploads

--- src/helpers/uploads/s3.ts
+++ src/helpers/uploads/s3.ts
@@ -1,5 +1,5 @@
 // Bun s3 client
-import { S3Client } from "bun";
+import { S3Client } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
     accessKeyId: Bun.env.SEVALLA_ACCESS_KEY_ID,
---
+++ src/helpers/uploads/uploadFile.ts
@@ -0,0 +1,12 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import s3Client from "./s3";
+
+export default async function uploadFile(file: File) {
+    const dataFileUrl = await s3Client.send(new PutObjectCommand({
+        Bucket: Bun.env.SEVALLA_BUCKET,
+        Key: `chatbots/${file.name}`,
+        Body: file
+    }));
+
+    return dataFileUrl.toString();
+}
