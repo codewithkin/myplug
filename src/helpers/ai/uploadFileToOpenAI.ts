0552f583fd49c2494d365987144cb0bea6e64146
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-05 10:23:22 +0200

    [ADD]: Reusable OpenAI client [UPDATE]: Upload file to openai function to use the new client

--- src/helpers/ai/uploadFileToOpenAI.ts
+++ src/helpers/ai/uploadFileToOpenAI.ts
@@ -1,15 +1,10 @@
 import fs from "fs";
-import OpenAI from "openai";
-
-const client = new OpenAI({
-    baseURL: 'https://api.studio.nebius.com/v1/',
-    apiKey: process.env.NEBIUS_API_KEY,
-});
+import openaiClient from ".";
 
 export async function uploadFileToOpenAI(filePath: string) {
     console.log("Uploading file to OpenAI: ", filePath);
 
-    const uploadedFile = await client.files.create({
+    const uploadedFile = await openaiClient.files.create({
         file: fs.createReadStream(filePath),
         purpose: "user_data",
     });
