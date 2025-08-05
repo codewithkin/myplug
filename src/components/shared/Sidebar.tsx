e660efbe45cf995014e49b6a1d13b653c5ea2761
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-05 13:43:40 +0200

    [ADD]: Mobile navbar

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,15 +1,70 @@
 "use client";
-import { Home, KeySquare, Settings } from "lucide-react"
+import { Home, KeySquare, Menu, Settings } from "lucide-react"
 import { ReactNode } from "react"
 import { Button } from "../ui/button"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
 
 type SidebarLink = {
     icon: ReactNode,
     name: string,
     href: string
+}
+
+function MobileAppSidebarContent () {
+        // TODO: ADD other links
+        const sidebarLinks: SidebarLink[] = [
+            {
+                icon: <Home />,
+                name: "Dashboard",
+                href: "/dashboard"
+            },
+            {
+                icon: <Settings />,
+                name: "Settings",
+                href: "/settings"
+            },
+            {
+                icon: <KeySquare />,
+                name: "API Keys",
+                href: "/api-keys"
+            },
+        ]
+
+    const currentPath = usePathname();
+
+    return (
+        <SheetContent side="left">
+            <SheetHeader>
+                <SheetTitle className="font-bold text-xl"><span className="text-blue-500">My</span>Plug</SheetTitle>
+            </SheetHeader>
+
+            <article className="p-4">
+                  {/* TODO: Add Shadcn Avatar */}
+
+                  <article className="flex flex-col gap-4">
+                        {
+                            sidebarLinks.map((link: SidebarLink, index: number) => {
+                                const pathNameIncluded = currentPath.includes(link.href);
+
+                                return (
+                                    <Button key={index} variant={pathNameIncluded ? "default" : "outline"} className={``} asChild>
+                                        <Link href={link.href}>
+                                            {link.icon}
+                                            {link.name}
+                                        </Link>
+                                    </Button>
+                                )
+                            })
+                        }
+                </article>
+
+                {/* TODO: Add secondary links */}
+            </article>
+        </SheetContent>
+    )
 }
 
 export default function Sidebar () {
@@ -47,7 +102,7 @@ export default function Sidebar () {
                                 const pathNameIncluded = currentPath.includes(link.href);
 
                                 return (
-                                    <Tooltip>
+                                    <Tooltip key={index}>
                                         <TooltipTrigger asChild>
                                             <Button variant={pathNameIncluded ? "default" : "outline"} className={``} asChild>
                                                 <Link href={link.href}>
@@ -66,6 +121,20 @@ export default function Sidebar () {
                 </article>
 
                 {/* TODO: Add secondary links */}
+            </article>
+
+            {/* Mobile navbar */}
+            <article className="w-full dark:bg-slate-900 bg-slate-200 p-4 flex items-center justify-between md:hidden">
+                <article className="flex gap-2 items-center">
+                    <Sheet>
+                        <SheetTrigger>
+                            <Menu strokeWidth={1.8} size={24} />
+                        </SheetTrigger>
+
+                        <MobileAppSidebarContent />
+                    </Sheet>
+                    <h2 className="text-xl font-bold"><span className="text-blue-500">My</span>Plug</h2>
+                </article>
             </article>
         </>
     )
