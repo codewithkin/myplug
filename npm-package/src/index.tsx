9336427dbc1842c0796296efd0eaa1bfff61e8d5
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-06 14:53:07 +0200

    [UPDATE]: Rename npm package to 'myplug-ai' and update configuration for build and dependencies

---
+++ npm-package/src/index.tsx
@@ -0,0 +1,7 @@
+export default function MyPlugAI() {
+    return (
+        <article>
+            <h1>MyPlugAI</h1>
+        </article>
+    )
+}
---
+++ npm-package/vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.tsx",
+      name: "MyPlugAI",
+      fileName: "my-plug-ai",
+      formats: ["es", "cjs"]
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        }
+      }
+    }
+  }
+});
