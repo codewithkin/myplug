d46e0be3b8c8cbf26a23bc2a580200708e2c1e87
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-12 13:44:29 +0200

    [UPDATE]: Extend chat store with actions for managing messages and chat state, including setMessages, addMessage, setChatId, and setChatBotId

--- npm-package/src/store/chatStore.ts
+++ npm-package/src/store/chatStore.ts
@@ -7,8 +7,19 @@ type State = {
     chatBotId?: string;
 }
 
-export const useChatStore = create<State>((set) => ({
+type Actions = {
+    setMessages: (messages: Message[]) => void;
+    addMessage: (message: Message) => void;
+    setChatId: (chatId: string) => void;
+    setChatBotId: (chatBotId: string) => void;
+}
+
+export const useChatStore = create<State & Actions>((set) => ({
     messages: [],
     chatId: undefined,
     chatBotId: undefined,
+    setMessages: (messages) => set({ messages }),
+    addMessage: (message) => set((state) => ({ messages: [...state.messages, message] })),
+    setChatId: (chatId) => set({ chatId }),
+    setChatBotId: (chatBotId) => set({ chatBotId }),
 }));
