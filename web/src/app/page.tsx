a9b8689d87ee6ab04a41ac2c53aad93c80808955
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-08-26 13:44:47 +0200

    [UPDATE]: Refactor ChatComponent integration to use environment variables for chatbot configuration

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -6,16 +6,16 @@ import Steps from "@/components/landing/Steps"
 import WhoItsForSection from "@/components/landing/WhoItsFor"
 import CtaSection from "@/components/landing/CTA"
 import Footer from "@/components/landing/shared/footer"
-import {ChatComponent} from "myplug-ai"
+import { ChatComponent } from "myplug-ai"
 
 function Home() {
     return (
         <article>
             <Header />
-            <ChatComponent 
-                chatBotId="59dac53c-d898-4a78-bf66-7e50721d6d3a" 
-                chatBotName="Kinly" 
-                website="https://codewithkin.space" 
+            <ChatComponent
+                chatBotId={process.env.NEXT_PUBLIC_CHATBOT_ID}
+                chatBotName={process.env.NEXT_PUBLIC_CHATBOT_NAME}
+                website={process.env.NEXT_PUBLIC_WEBSITE}
                 apiKey={process.env.NEXT_PUBLIC_MYPLUG_API_KEY} />
             <Steps />
             <FeaturesSection />
